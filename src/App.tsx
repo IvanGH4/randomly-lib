e3a579f03eced7b04afb8bc907381b1a97cb66ae
Ivan Garcia <ivangarciaharriague@gmail.com>
2023-03-18 00:23:02 -0300

    Tasks by difficulty brought to you by ChatGPT

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import FormInput from './components/FormInput';
 
 type Result = {
   name: string;
-  task: string;
+  task: Task;
+};
+
+type Task = {
+  name: string;
+  difficulty: number;
 };
 
 type State = {
-  tasks: string[];
+  tasks: Task[];
   participants: string[];
-  task: string;
+  task: Task;
   participant: string;
   results: Result[];
 };
@@ -29,37 +34,85 @@ type Action = {
     | string
     | {
         key: 'task' | 'participant';
-        value: string;
-      };
+        value: string | Task;
+      }
+    | Task;
 };
 
-const shuffle = (array: string[]): string[] => {
-  return array.sort(() => Math.random() - 0.5);
-};
+// const shuffle = (array: string[]): string[] => {
+//   return array.sort(() => Math.random() - 0.5);
+// };
 
-const assignTasks = (participants: string[], tasks: string[]): Result[] => {
-  const tasksAux = [...tasks];
+const assignTasks = (participants: string[], tasks: Task[]): Result[] => {
+  // const tasksAux = [...tasks];
+  // const results: Result[] = [];
+  // // TODO: niveles de dificultad a las tareas
+  // while (tasks.length > results.length) {
+  //   participants.forEach((name) => {
+  //     const task =
+  //       shuffle(tasksAux)[Math.floor(Math.random() * tasksAux.length)];
+  //     results.push({
+  //       name,
+  //       task: task || 'Zafaste!',
+  //     });
+  //     tasksAux.splice(tasksAux.indexOf(task), 1);
+  //   });
+  // }
+
+  // return results as Result[];
+  const taskDifficulties = Array.from(
+    new Set(tasks.map((task) => task.difficulty))
+  );
+  const tasksByDifficulty: { [difficulty: number]: Task[] } = {};
+  taskDifficulties.forEach((difficulty) => {
+    tasksByDifficulty[difficulty] = tasks.filter(
+      (task) => task.difficulty === difficulty
+    );
+  });
+
+  const taskCountsByDifficulty: { [difficulty: number]: number } = {};
+  taskDifficulties.forEach((difficulty) => {
+    taskCountsByDifficulty[difficulty] = tasksByDifficulty[difficulty].length;
+  });
+
+  const participantTasks: { [name: string]: Task[] } = {};
+  participants.forEach((name) => {
+    participantTasks[name] = [];
+  });
+
   const results: Result[] = [];
-  // TODO: niveles de dificultad a las tareas
-  while (tasks.length > results.length) {
-    participants.forEach((name) => {
+  for (
+    let difficultyIndex = 0;
+    difficultyIndex < taskDifficulties.length;
+    difficultyIndex++
+  ) {
+    const difficulty = taskDifficulties[difficultyIndex];
+    for (
+      let participantIndex = 0;
+      participantIndex < participants.length;
+      participantIndex++
+    ) {
+      const currentParticipant = participants[participantIndex];
+      const availableTasks = tasksByDifficulty[difficulty].filter(
+        (task) => !participantTasks[currentParticipant].includes(task)
+      );
       const task =
-        shuffle(tasksAux)[Math.floor(Math.random() * tasksAux.length)];
+        availableTasks[Math.floor(Math.random() * availableTasks.length)];
+      participantTasks[currentParticipant].push(task);
       results.push({
-        name,
-        task: task || 'Zafaste!',
+        name: currentParticipant,
+        task,
       });
-      tasksAux.splice(tasksAux.indexOf(task), 1);
-    });
+    }
   }
 
-  return results as Result[];
+  return results;
 };
 
 const initialState: State = {
   tasks: [],
   participants: [],
-  task: '',
+  task: {} as Task,
   participant: '',
   results: [],
 };
@@ -73,13 +126,23 @@ const reducer = (state: State, action: Action): State => {
         participants: [...state.participants, action.payload as string],
       };
     case 'SET_TASK':
-      return {
-        ...state,
-        task: '',
-        tasks: [...state.tasks, action.payload as string],
-      };
+      if (typeof action.payload === 'object' && 'name' in action.payload) {
+        return {
+          ...state,
+          task: { name: '', difficulty: 0 },
+          // tasks: [...state.tasks, action.payload as string],
+          tasks: [
+            ...state.tasks,
+            {
+              name: action.payload?.name,
+              difficulty: action.payload?.difficulty,
+            },
+          ],
+        };
+      }
+      return state;
     case 'ON_CHANGE':
-      if (typeof action.payload === 'object') {
+      if (typeof action.payload === 'object' && 'key' in action.payload) {
         return { ...state, [action.payload?.key]: action.payload?.value };
       }
       return state;
@@ -99,16 +162,6 @@ const reducer = (state: State, action: Action): State => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const handleEditParticipant = (participant: string, idx: number) => {
-    // dispatch({
-    //   type: 'ON_CHANGE',
-    //   payload: {
-    //     key: 'participant',
-    //     value: participant,
-    //   },
-    // })
-  }
 
   return (
     <main className='px-5 py-5 md:px-20 bg-[#333333] min-h-screen'>
@@ -135,7 +188,10 @@ function App() {
                     })
                   }
                   handleClick={() => {
-                    if (state.participant !== '' && /^[a-zA-Z0-9\s-?]+$/.test(state.participant)) {
+                    if (
+                      state.participant !== '' &&
+                      /^[a-zA-Z0-9\s-?]+$/.test(state.participant)
+                    ) {
                       dispatch({
                         type: 'SET_PARTICIPANT',
                         payload: state.participant,
@@ -147,22 +203,36 @@ function App() {
 
               <form
                 onSubmit={(e) => e.preventDefault()}
-                className='col-span-1 flex flex-col gap-10'
+                className='col-span-1 flex flex-col gap-x-10'
               >
+                <p className='text-sm text-gray-400 mb-3'>
+                  To add a difficulty to a task you have to use the following
+                  syntax:
+                  <br />
+                  <code>`task name - difficulty`</code>
+                </p>
                 <FormInput
-                  value={state.task}
+                  value={state.task.name}
                   type='task'
                   handleChange={(e: ChangeEvent<HTMLInputElement>) => {
                     dispatch({
                       type: 'ON_CHANGE',
                       payload: {
                         key: 'task',
-                        value: e.target.value,
+                        value: {
+                          name: e.target.value,
+                          difficulty:
+                            Number(e.target.value?.split('-').pop()) ||
+                            Math.floor(Math.random() * 6),
+                        },
                       },
                     });
                   }}
                   handleClick={() => {
-                    if (state.task !== '' && /^[a-zA-Z0-9\s-]+$/.test(state.task)) {
+                    if (
+                      state.task.name !== '' &&
+                      /^[a-zA-Z0-9\s-]+$/.test(state.task.name)
+                    ) {
                       dispatch({
                         type: 'SET_TASK',
                         payload: state.task,
@@ -210,7 +280,7 @@ function App() {
               state.results.map((result, i) => (
                 <div key={i}>
                   <p>
-                    {result.name}: {result.task}
+                    {result.name}: {result.task?.name}
                   </p>
                 </div>
               ))
@@ -231,7 +301,10 @@ function App() {
                 <h3 className='mt-10 text-xl text-white'>Tasks</h3>
                 <ul>
                   {state.tasks.map((t) => (
-                    <li key={t}>{t}</li>
+                    <li key={t.name}>
+                      {t.name.slice(0, t.name.lastIndexOf('-'))} - Difficulty:{' '}
+                      {t.difficulty}
+                    </li>
                   ))}
                 </ul>
               </>
