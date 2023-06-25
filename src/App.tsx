6bf589e566d7bd5f4b011c7151ff3f155d91736c
ivangh4 <ivangarciaharriague@gmail.com>
2023-06-25 00:22:06 -0300

    Back to make it work with simple logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent, useReducer } from 'react';
+import { ChangeEvent, useReducer } from "react";
 
-import Cta from './components/Cta';
-import FormInput from './components/FormInput';
+import Cta from "./components/Cta";
+import FormInput from "./components/FormInput";
 
 type Result = {
   name: string;
@@ -23,176 +23,65 @@ type State = {
 
 type Action = {
   type:
-    | 'SET_PARTICIPANT'
-    | 'SET_TASK'
-    | 'ON_CHANGE'
-    | 'CLEAR_ALL'
-    | 'RESET_WITH_PARTICIPANTS'
-    | 'RESET_WITH_TASKS'
-    | 'GET_RESULTS';
+    | "SET_PARTICIPANT"
+    | "SET_TASK"
+    | "ON_CHANGE"
+    | "CLEAR_ALL"
+    | "RESET_WITH_PARTICIPANTS"
+    | "RESET_WITH_TASKS"
+    | "GET_RESULTS";
   payload?:
     | string
     | {
-        key: 'task' | 'participant';
+        key: "task" | "participant";
         value: string | Task;
       }
     | Task;
 };
 
-// const shuffle = (array: string[]): string[] => {
-//   return array.sort(() => Math.random() - 0.5);
-// };
-
-// const assignTasks = (participants: string[], tasks: Task[]): Result[] => {
-//   // const tasksAux = [...tasks];
-//   // const results: Result[] = [];
-//   // // TODO: niveles de dificultad a las tareas
-//   // while (tasks.length > results.length) {
-//   //   participants.forEach((name) => {
-//   //     const task =
-//   //       shuffle(tasksAux)[Math.floor(Math.random() * tasksAux.length)];
-//   //     results.push({
-//   //       name,
-//   //       task: task || 'Zafaste!',
-//   //     });
-//   //     tasksAux.splice(tasksAux.indexOf(task), 1);
-//   //   });
-//   // }
-
-//   // return results as Result[];
-//   const taskDifficulties = Array.from(
-//     new Set(tasks.map((task) => task.difficulty))
-//   );
-//   const tasksByDifficulty: { [difficulty: number]: Task[] } = {};
-//   taskDifficulties.forEach((difficulty) => {
-//     tasksByDifficulty[difficulty] = tasks.filter(
-//       (task) => task.difficulty === difficulty
-//     );
-//   });
-
-//   const taskCountsByDifficulty: { [difficulty: number]: number } = {};
-//   taskDifficulties.forEach((difficulty) => {
-//     taskCountsByDifficulty[difficulty] = tasksByDifficulty[difficulty].length;
-//   });
-
-//   const participantTasks: { [name: string]: Task[] } = {};
-//   participants.forEach((name) => {
-//     participantTasks[name] = [];
-//   });
-
-//   const results: Result[] = [];
-//   for (
-//     let difficultyIndex = 0;
-//     difficultyIndex < taskDifficulties.length;
-//     difficultyIndex++
-//   ) {
-//     const difficulty = taskDifficulties[difficultyIndex];
-//     for (
-//       let participantIndex = 0;
-//       participantIndex < participants.length;
-//       participantIndex++
-//     ) {
-//       const currentParticipant = participants[participantIndex];
-//       console.log('participantTasks[currentParticipant]', participantTasks[currentParticipant]);
-//       console.log('tasksByDifficulty[difficulty]', tasksByDifficulty[difficulty]);
-      
-//       const availableTasks = tasksByDifficulty[difficulty].filter(
-//         (task) => !participantTasks[currentParticipant].includes(task)
-//       );
-//       const task =
-//         availableTasks[Math.floor(Math.random() * availableTasks.length)];
-//       participantTasks[currentParticipant].push(task);
-//       results.push({
-//         name: currentParticipant,
-//         task,
-//       });
-//     }
-//   }
-
-//   return results;
-// };
-
-const assignTasks = (participants: string[], tasks: Task[]): Result[] => {
-  const taskDifficulties = Array.from(
-    new Set(tasks.map((task) => task.difficulty))
-  );
-  const tasksByDifficulty: { [difficulty: number]: Task[] } = {};
-  taskDifficulties.forEach((difficulty) => {
-    tasksByDifficulty[difficulty] = tasks.filter(
-      (task) => task.difficulty === difficulty
-    );
-  });
-
-  const taskCountsByDifficulty: { [difficulty: number]: number } = {};
-  taskDifficulties.forEach((difficulty) => {
-    taskCountsByDifficulty[difficulty] = tasksByDifficulty[difficulty].length;
-  });
-
-  const participantTasks: { [name: string]: Task[] } = {};
-  participants.forEach((name) => {
-    participantTasks[name] = [];
-  });
-
-  const assignedTasks: Task[] = [];
-
-  const results: Result[] = [];
-  for (
-    let difficultyIndex = 0;
-    difficultyIndex < taskDifficulties.length;
-    difficultyIndex++
-  ) {
-    const difficulty = taskDifficulties[difficultyIndex];
-    for (
-      let participantIndex = 0;
-      participantIndex < participants.length;
-      participantIndex++
-    ) {
-      const currentParticipant = participants[participantIndex];
-
-      const availableTasks = tasksByDifficulty[difficulty].filter(
-        (task) =>
-          !participantTasks[currentParticipant].includes(task) &&
-          !assignedTasks.includes(task)
-      );
-      if (availableTasks.length === 0) {
-        continue;
-      }
-      const task =
-        availableTasks[Math.floor(Math.random() * availableTasks.length)];
-      participantTasks[currentParticipant].push(task);
-      assignedTasks.push(task);
-      results.push({
-        name: currentParticipant,
-        task,
-      });
-    }
-  }
-
-  return results;
+const shuffle = (array: string[]): string[] => {
+  return array.sort(() => Math.random() - 0.5);
 };
 
+const assignTasks = (participants: string[], tasks: Task[]): Result[] => {
+  const tasksAux = [...tasks];
+  const results: Result[] = [];
+
+  while (tasks.length > results.length) {
+    shuffle(participants).forEach((name) => {
+      const task = tasksAux[Math.floor(Math.random() * tasksAux.length)];
+      results.push({
+        name,
+        task: task || { name: "Zafaste!", difficulty: 0 },
+      });
+      tasksAux.splice(tasksAux.indexOf(task), 1);
+    });
+  }
+
+  return results as Result[];
+};
 
 const initialState: State = {
   tasks: [],
   participants: [],
   task: {} as Task,
-  participant: '',
+  participant: "",
   results: [],
 };
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case 'SET_PARTICIPANT':
+    case "SET_PARTICIPANT":
       return {
         ...state,
-        participant: '',
+        participant: "",
         participants: [...state.participants, action.payload as string],
       };
-    case 'SET_TASK':
-      if (typeof action.payload === 'object' && 'name' in action.payload) {
+    case "SET_TASK":
+      if (typeof action.payload === "object" && "name" in action.payload) {
         return {
           ...state,
-          task: { name: '', difficulty: 0 },
+          task: { name: "", difficulty: 0 },
           // tasks: [...state.tasks, action.payload as string],
           tasks: [
             ...state.tasks,
@@ -204,18 +93,18 @@ const reducer = (state: State, action: Action): State => {
         };
       }
       return state;
-    case 'ON_CHANGE':
-      if (typeof action.payload === 'object' && 'key' in action.payload) {
+    case "ON_CHANGE":
+      if (typeof action.payload === "object" && "key" in action.payload) {
         return { ...state, [action.payload?.key]: action.payload?.value };
       }
       return state;
-    case 'CLEAR_ALL':
+    case "CLEAR_ALL":
       return initialState;
-    case 'RESET_WITH_PARTICIPANTS':
+    case "RESET_WITH_PARTICIPANTS":
       return { ...initialState, participants: state.participants };
-    case 'RESET_WITH_TASKS':
+    case "RESET_WITH_TASKS":
       return { ...initialState, tasks: state.tasks };
-    case 'GET_RESULTS':
+    case "GET_RESULTS":
       const results: Result[] = assignTasks(state.participants, state.tasks);
       return { ...state, results };
     default:
@@ -227,36 +116,36 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <main className='px-5 py-5 md:px-20 bg-[#333333] min-h-screen'>
-      <h1 className='text-4xl font-semibold text-white'>Randomly!</h1>
+    <main className="px-5 py-5 md:px-20 bg-[#333333] min-h-screen">
+      <h1 className="text-4xl font-semibold text-white">Randomly!</h1>
 
-      <section className='mt-20 grid grid-cols-1 lg:grid-cols-2'>
+      <section className="mt-20 grid grid-cols-1 lg:grid-cols-2">
         <article>
           {!state.results.length && (
             <>
               <form
                 onSubmit={(e) => e.preventDefault()}
-                className='col-span-1 flex flex-col gap-10 mb-10'
+                className="col-span-1 flex flex-col gap-10 mb-10"
               >
                 <FormInput
                   value={state.participant}
-                  type='participant'
+                  type="participant"
                   handleChange={(e: ChangeEvent<HTMLInputElement>) =>
                     dispatch({
-                      type: 'ON_CHANGE',
+                      type: "ON_CHANGE",
                       payload: {
-                        key: 'participant',
+                        key: "participant",
                         value: e.target.value,
                       },
                     })
                   }
                   handleClick={() => {
                     if (
-                      state.participant !== '' &&
+                      state.participant !== "" &&
                       /^[a-zA-Z0-9\s-?]+$/.test(state.participant)
                     ) {
                       dispatch({
-                        type: 'SET_PARTICIPANT',
+                        type: "SET_PARTICIPANT",
                         payload: state.participant,
                       });
                     }
@@ -266,9 +155,9 @@ function App() {
 
               <form
                 onSubmit={(e) => e.preventDefault()}
-                className='col-span-1 flex flex-col gap-x-10'
+                className="col-span-1 flex flex-col gap-x-10"
               >
-                <p className='text-sm text-gray-400 mb-3'>
+                <p className="text-sm text-gray-400 mb-3">
                   To add a difficulty to a task you have to use the following
                   syntax:
                   <br />
@@ -276,16 +165,16 @@ function App() {
                 </p>
                 <FormInput
                   value={state.task.name}
-                  type='task'
+                  type="task"
                   handleChange={(e: ChangeEvent<HTMLInputElement>) => {
                     dispatch({
-                      type: 'ON_CHANGE',
+                      type: "ON_CHANGE",
                       payload: {
-                        key: 'task',
+                        key: "task",
                         value: {
                           name: e.target.value,
                           difficulty:
-                            Number(e.target.value?.split('-').pop()) ||
+                            Number(e.target.value?.split("-").pop()) ||
                             Math.floor(Math.random() * 6),
                         },
                       },
@@ -293,11 +182,11 @@ function App() {
                   }}
                   handleClick={() => {
                     if (
-                      state.task.name !== '' &&
+                      state.task.name !== "" &&
                       /^[a-zA-Z0-9\s-]+$/.test(state.task.name)
                     ) {
                       dispatch({
-                        type: 'SET_TASK',
+                        type: "SET_TASK",
                         payload: state.task,
                       });
                     }
@@ -307,38 +196,38 @@ function App() {
             </>
           )}
 
-          <div className='mt-20 grid grid-cols-2 max-w-xl gap-5'>
+          <div className="mt-20 grid grid-cols-2 max-w-xl gap-5">
             <Cta
-              text='Get results'
-              type='success'
-              handleClick={() => dispatch({ type: 'GET_RESULTS' })}
+              text="Get results"
+              type="success"
+              handleClick={() => dispatch({ type: "GET_RESULTS" })}
               btnDisabled={state.results.length > 0}
             />
             <Cta
-              text='Clear all'
-              type='error'
-              handleClick={() => dispatch({ type: 'CLEAR_ALL' })}
+              text="Clear all"
+              type="error"
+              handleClick={() => dispatch({ type: "CLEAR_ALL" })}
             />
             <Cta
-              text='Clear tasks'
-              type='error'
-              handleClick={() => dispatch({ type: 'RESET_WITH_PARTICIPANTS' })}
+              text="Clear tasks"
+              type="error"
+              handleClick={() => dispatch({ type: "RESET_WITH_PARTICIPANTS" })}
             />
             <Cta
-              text='Clear participants'
-              type='error'
-              handleClick={() => dispatch({ type: 'RESET_WITH_TASKS' })}
+              text="Clear participants"
+              type="error"
+              handleClick={() => dispatch({ type: "RESET_WITH_TASKS" })}
             />
           </div>
         </article>
 
-        <div className='col-span-1'>
-          <h2 className='text-white text-2xl font-medium mb-4'>
+        <div className="col-span-1">
+          <h2 className="text-white text-2xl font-medium mb-4">
             {!state.results.length
-              ? 'Add some participants and tasks and then click the Get results button!'
-              : 'Results are here! Now get to work!'}
+              ? "Add some participants and tasks and then click the Get results button!"
+              : "Results are here! Now get to work!"}
           </h2>
-          <div className='text-gray-400'>
+          <div className="text-gray-400">
             {state.results.length ? (
               state.results.map((result, i) => (
                 <div key={i}>
@@ -349,10 +238,10 @@ function App() {
               ))
             ) : (
               <>
-                <h3 className='mt-10 text-xl text-white'>Participants</h3>
+                <h3 className="mt-10 text-xl text-white">Participants</h3>
                 <ul>
                   {state.participants.map((p, idx) => (
-                    <div className='flex items-center gap-1'>
+                    <div className="flex items-center gap-1">
                       {/* onClick={() => handleEditParticipant(p, idx)} */}
                       {/* <button>
                         <img src="/edit.png" alt="Edit icon" className='max-w-[20px]' />
@@ -361,11 +250,11 @@ function App() {
                     </div>
                   ))}
                 </ul>
-                <h3 className='mt-10 text-xl text-white'>Tasks</h3>
+                <h3 className="mt-10 text-xl text-white">Tasks</h3>
                 <ul>
                   {state.tasks.map((t) => (
                     <li key={t.name}>
-                      {t.name.slice(0, t.name.lastIndexOf('-'))} - Difficulty:{' '}
+                      {t.name.slice(0, t.name.lastIndexOf("-"))} - Difficulty:{" "}
                       {t.difficulty}
                     </li>
                   ))}
